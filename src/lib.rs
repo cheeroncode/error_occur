cc0d72745d31d490cbefa0e9cc51cc1d73b51292
cheeroncode <code@autodo.xyz>
2021-05-04 00:47:53 +0800

    添加 ErrorOccur.error 方法返回值的关联类型 使用中文简要解释说明方法

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, str};
 
 mod cause;
 mod remind;
@@ -8,16 +8,25 @@ use cause::ErrorCause;
 #[doc(inline)]
 pub use remind::ErrorRemind;
 /**
-## Defines a method to create [`ErrorRemind`] directly.
+## 定义发生错误时创建错误的方法。
+Defines a method to create an error when it occurs.
 */
-pub trait ErrorOccur<T> {
-    /// Define the error `ErrorRemind` directly.
-    fn error(self, message: &str) -> std::result::Result<T, ErrorRemind>;
+pub trait ErrorOccur<T, M = &'static str> {
+    type Output;
+    /**
+    使用`message`创建错误。
+    Use `message` to create the error.
+    */
+    fn error(self, message: M) -> Self::Output;
 }
 
 impl<T> ErrorOccur<T> for Option<T> {
-    /// Define the error `ErrorRemind` directly with `Option`
-    fn error(self, message: &str) -> std::result::Result<T, ErrorRemind> {
+    type Output = std::result::Result<T, ErrorRemind>;
+    /**
+    ## 如果是[`Option::None`]，使用`message`创建错误[`ErrorRemind`]。
+    If it is [`Option::None`], use `message` to create the error [`ErrorRemind`].
+    */
+    fn error(self, message: &str) -> Self::Output {
         match self {
             Some(v) => Ok(v),
             None => Err(message.into()),
@@ -26,8 +35,12 @@ impl<T> ErrorOccur<T> for Option<T> {
 }
 
 impl<T, E: Error> ErrorOccur<T> for std::result::Result<T, E> {
-    /// Define the error `ErrorRemind` directly with `Result`
-    fn error(self, message: &str) -> Result<T, ErrorRemind> {
+    type Output = std::result::Result<T, ErrorRemind>;
+    /**
+    ## 如果是[`Result::Err`]，使用`message`创建错误[`ErrorRemind`]。
+    If it is [`Result::Err`], use `message` to create the error [`ErrorRemind`].
+    */
+    fn error(self, message: &str) -> Self::Output {
         match self {
             Ok(v) => Ok(v),
             Err(ref e) => Err(ErrorRemind::new(message).cause(e)),
@@ -36,7 +49,11 @@ impl<T, E: Error> ErrorOccur<T> for std::result::Result<T, E> {
 }
 
 impl ErrorOccur<bool> for bool {
-    /// Define the error `ErrorRemind` directly with `bool`
+    type Output = std::result::Result<bool, ErrorRemind>;
+    /**
+    ## 如果是[`false`]，使用`message`创建错误[`ErrorRemind`]。
+    If it is [`false`], use `message` to create the error [`ErrorRemind`].
+    */
     fn error(self, message: &str) -> std::result::Result<bool, ErrorRemind> {
         match self {
             true => Ok(true),
@@ -46,20 +63,24 @@ impl ErrorOccur<bool> for bool {
 }
 
 impl<T> ErrorOccur<T> for &'static str {
+    type Output = std::result::Result<T, ErrorRemind>;
     /**
-    Define the error `ErrorRemind` directly with `&'static str`.
+    ## 使用`＆'static str`直接创建错误[`ErrorRemind`]。
+
+    Use `&'static str` to create the error [`ErrorRemind`] directly.
 
     It always returns the value `Err`.
 
     **NOTE** : So, do not use `match` for the return value, you will see the following error, and this is intentional.
     ```
     /*
+    hint error:
     type annotations needed
     cannot infer type for type parameter `T` declared on the trait `ErrorOccur`rustc(E0282)
     */
     ```
     */
-    fn error(self, message: &str) -> Result<T, ErrorRemind> {
+    fn error(self, message: &str) -> Self::Output {
         if self.trim().is_empty() {
             Err(ErrorRemind::new(&format!("{}", message)))
         } else {
@@ -69,8 +90,11 @@ impl<T> ErrorOccur<T> for &'static str {
 }
 
 impl<T> ErrorOccur<T> for String {
+    type Output = std::result::Result<T, ErrorRemind>;
     /**
-    Define the error `ErrorRemind` directly with `String`.
+    ## 使用[`String`]直接创建错误[`ErrorRemind`]。
+
+    Use [`String`] to create the error [`ErrorRemind`] directly.
 
     It always returns the value `Err`.
 
@@ -82,7 +106,7 @@ impl<T> ErrorOccur<T> for String {
     */
     ```
     */
-    fn error(self, message: &str) -> Result<T, ErrorRemind> {
+    fn error(self, message: &str) -> Self::Output {
         if self.trim().is_empty() {
             Err(ErrorRemind::new(&format!("{}", message)))
         } else {
